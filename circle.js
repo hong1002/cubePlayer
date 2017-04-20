66a4f41751cfab6d8b8fdc6a85e1451d22080b8a
hong1002 <2743931164@qq.com>
2017-04-20 14:27:14 +0800

    添加拖动进度条功能
    进度条更换为 jQuery 插件Round Slider

--- circle.js
+++ circle.js
@@ -1,35 +1,36 @@
-var music = document.querySelector('#music')
+var audio = document.querySelector('#music')
 //绑定timeupdate事件
-music.addEventListener('timeupdate',function(){
-    if (!isNaN(music.duration)) {
-        var progressValue = music.currentTime/music.duration; //用时间比来获取进度条的值
-        if(progressValue == 1){
-            progressValue=0;//当播放完成，进度条跳到开始
-        }
-    drawCircle(canvas,progressValue);
-    };
-},false);
-
-drawCircle = function (canvas, percentage) {
-    var canvasWidth = 300;
-    var innerR = canvasWidth * 0.8 * 0.5;//半径
-    var ctx;
-    canvas.setAttribute('width', canvasWidth + 'px');
-    canvas.setAttribute('height', canvasWidth + 'px');
-    if (canvas.getContext) {
-        ctx = canvas.getContext('2d');
+$("#slider").roundSlider({
+    handleSize: "1,1",
+    width: 22,
+    radius: 100,
+    value: 0,
+    max: "100",
+    startAngle: 90,
+    step: "0.005",
+    showTooltip: false,
+    editableTooltip: false,
+    sliderType: "min-range",
+    drag: function (args) {
+        seek(args);
+    },
+    change: function (args) {
+        // handle the change event here
+        seek(args);
     }
-    ctx.translate(canvasWidth / 2, canvasWidth / 2);
-    ctx.beginPath();
-    ctx.arc(0, 0, innerR, 0, Math.PI * 2, false);
-    ctx.lineWidth = 15;
-    ctx.strokeStyle = "#24666A";
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.arc(0, 0, innerR, Math.PI * 3 / 2, (Math.PI * 3 / 2 + Math.PI * 2 / 180 + percentage * Math.PI * 2), false);
-    ctx.lineWidth = 16;
-    ctx.strokeStyle = "#D86750";
-    ctx.stroke();
-};
+    });
 
-drawCircle(document.getElementById('canvas'),0);
+    $(audio).on("timeupdate", function(){
+        seektimeupdate();
+    });
+
+    function seektimeupdate(){
+        var nt = audio.currentTime * (100 / audio.duration);
+        $("#slider").roundSlider("option", "value", nt);
+    }
+    function seek(event){
+        var val = event.value;
+        $("#slider").roundSlider("option", "value", val);
+        seekto = audio.duration * (val / 100);
+        audio.currentTime = seekto;
+    }
